f06fd38a30852b8b54cbaa42bd1d91f334964b58
AndryAmagua <andryamagua5@gmail.com>
2023-07-28 15:35:17 -0500

    edit view parametros

--- app/parametros/page.jsx
+++ app/parametros/page.jsx
@@ -6,20 +6,33 @@ import CardPeriodo from "@/components/cards/CardPeriodo"
 function Parametros() {
     return (
         <div className="container-sm p-3">
-            <h1 style={{ textAlign: 'center' }}>Parametros de solicitudes</h1>
-            <div className="row">
-                <div className="col-sm-6">
+            <h1 style={{ textAlign: 'center' }}>Parámetros de solicitudes</h1>
+
+            <ul className="nav nav-tabs" role="tablist">
+                <li className="nav-item" role="presentation">
+                    <a className="nav-link" data-bs-toggle="tab" href="#carreras" role="tab">CARRERAS</a>
+                </li>
+                <li className="nav-item" role="presentation">
+                    <a className="nav-link" data-bs-toggle="tab" href="#niveles" role="tab">NIVELES</a>
+                </li>
+                <li className="nav-item" role="presentation">
+                    <a className="nav-link" data-bs-toggle="tab" href="#paralelos" role="tab" >PARALELOS</a>
+                </li>
+                <li className="nav-item" role="presentation">
+                    <a className="nav-link" data-bs-toggle="tab" href="#periodos" role="tab">PERIODOS</a>
+                </li>
+            </ul>
+            <div id="myTabContent" className="tab-content">
+                <div className="tab-pane fade p-3" id="carreras" role="tabpanel">
                     <CardCarrera />
                 </div>
-                <div className="col-sm-6">
+                <div className="tab-pane fade p-3" id="niveles" role="tabpanel">
                     <CardNivel />
                 </div>
-            </div>
-            <div className="row">
-                <div className="col-sm-6">
+                <div className="tab-pane fade p-3" id="paralelos" role="tabpanel">
                     <CardParalelo />
                 </div>
-                <div className="col-sm-6">
+                <div className="tab-pane fade p-3" id="periodos" role="tabpanel">
                     <CardPeriodo />
                 </div>
             </div>
