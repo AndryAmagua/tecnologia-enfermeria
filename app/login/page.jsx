ea5f059ce307563d472f6ca68a45219d8baa32c7
AndryAmagua <andryamagua5@gmail.com>
2023-07-29 13:45:21 -0500

    login view

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,11 +1,31 @@
 'use client'
 import { signIn } from "next-auth/react"
+import Image from "next/image"
+import logoPucesi from '@/public/logo_pucesi.png'
 
 function Login() {
     return (
-        <div>
-            <button onClick={() => signIn()}>Sign in</button>
-        </div>  
+        <div className="d-flex align-items-center justify-content-center vh-100"
+            style={{ backgroundImage: 'url(https://p1.pxfuel.com/preview/1006/270/445/blue-water-shimmer-gradient-white-light.jpg)', backgroundSize: 'cover' }}
+        >
+            <div className="row align-items-center gx-5 m-5">
+                <div className="col-md-6 my-3">
+                    <Image src={logoPucesi} className="img-fluid img-thumbnail" alt="PUCESI" />
+                </div>
+                <div className="col-md-6 my-3">
+                    <div className="mx-3 py-5 px-4 rounded bg-primary">
+                        <h3 className="text-light text-center mb-3">Sistema de control de reservas para enfermería</h3>
+                        <div className="mb-3 align-self-center">
+                            <input type="text" className="form-control" placeholder="Usuario" />
+                        </div>
+                        <div className="mb-3">
+                            <input type="password" className="form-control" placeholder="Contraseña" />
+                        </div>
+                        <button type="submit" className="btn btn-light">Iniciar sesión</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,48 +4,50 @@ import { useSession, signOut } from "next-auth/react"
 
 function Navbar() {
     const { data: session, status } = useSession()
+    if (session) {
+        return (
+            <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+                <div className="container-fluid">
+                    <h4 className="navbar-brand">PUCE TEC</h4>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarColor01">
+                        <ul className="navbar-nav me-auto">
+                            <li className="nav-item">
+                                <Link className="nav-link" href="/">Inventario</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" href="/insumos">Insumos</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" href="/laboratorios">Laboratorios</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" href="/equipos">Equipos</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" href="/reservas">Reservas</Link>
+                            </li>
+                        </ul>
 
-    return (
-        <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
-            <div className="container-fluid">
-                <h4 className="navbar-brand">PUCE TEC</h4>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarColor01">
-                    <ul className="navbar-nav me-auto">
-                        <li className="nav-item">
-                            <Link className="nav-link" href="/">Inventario</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" href="/insumos">Insumos</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" href="/laboratorios">Laboratorios</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" href="/equipos">Equipos</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" href="/reservas">Reservas</Link>
-                        </li>
-                    </ul>
-
-                    <div className="btn-group" role="group" aria-label="Button group with nested dropdown">
-                        <div className="btn-group" role="group">
-                            <button id="btnGroupDrop1" className="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            </button>
-                            <div className="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                                <a className="dropdown-item" onClick={() => signOut()}>Cerrar sesión</a>
-                                <Link className="dropdown-item" href="/parametros">Ajustes de parámetros</Link>
+                        <div className="btn-group" role="group" aria-label="Button group with nested dropdown">
+                            <div className="btn-group" role="group">
+                                <button id="btnGroupDrop1" className="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                </button>
+                                <div className="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                                    <a className="dropdown-item" onClick={() => signOut()}>Cerrar sesión</a>
+                                    <Link className="dropdown-item" href="/parametros">Ajustes de parámetros</Link>
+                                </div>
                             </div>
+                            <button type="button" className="btn btn-primary disabled">Cuenta administrador</button>
                         </div>
-                        <button type="button" className="btn btn-primary disabled">Cuenta administrador</button>
                     </div>
                 </div>
-            </div>
-        </nav>
-    )
+            </nav>
+        )
+    }
+
 }
 
 export default Navbar
