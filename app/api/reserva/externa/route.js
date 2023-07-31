d66af2d2132b97e29f61cb54ace5487cf9c64554
AndryAmagua <andryamagua5@gmail.com>
2023-07-30 23:23:18 -0500

    remove tipo reserva

--- app/api/reserva/externa/route.js
+++ app/api/reserva/externa/route.js
@@ -5,7 +5,7 @@ export async function GET(request) {
     const db = await openDB()
     if (db.estado === undefined) {
         db.connect()
-        const [rows, fields] = await db.query('SELECT A.SOE_ID, A.SOE_GUIA, A.SOE_ASISTENTES, A.SOE_FECHA, A.SOE_HORA_INGRESO, A.SOE_HORA_SALIDA, A.SOE_OBSERVACION, A.SOE_ESTADO, B.TIR_NOMBRE, C.PER_NOMBRES, C.PER_APELLIDOS, C.PER_INSTITUCION, D.PEA_NOMBRE, E.LAB_NOMBRE FROM tbl_solicitud_externa AS A INNER JOIN tbl_tipo_reserva AS B ON A.TIR_ID = B.TIR_ID INNER JOIN tbl_personal_externo AS C ON A.PER_ID = C.PER_ID INNER JOIN tbl_periodo_academico AS D ON A.PEA_ID = D.PEA_ID INNER JOIN tbl_laboratorios AS E ON A.LAB_ID = E.LAB_ID ORDER BY A.SOE_ID DESC')
+        const [rows, fields] = await db.query('SELECT A.SOE_ID, A.SOE_GUIA, A.SOE_ASISTENTES, A.SOE_FECHA, A.SOE_HORA_INGRESO, A.SOE_HORA_SALIDA, A.SOE_OBSERVACION, A.SOE_ESTADO, C.PER_NOMBRES, C.PER_APELLIDOS, C.PER_INSTITUCION, D.PEA_NOMBRE, E.LAB_NOMBRE FROM tbl_solicitud_externa AS A INNER JOIN tbl_personal_externo AS C ON A.PER_ID = C.PER_ID INNER JOIN tbl_periodo_academico AS D ON A.PEA_ID = D.PEA_ID INNER JOIN tbl_laboratorios AS E ON A.LAB_ID = E.LAB_ID ORDER BY A.SOE_ID DESC')
         db.end()
         return NextResponse.json({ data: rows }, { status: 200 })
 
@@ -17,8 +17,8 @@ export async function GET(request) {
 export async function POST(request) {
     const db = await openDB()
     db.connect()
-    const { tipoSolicitud, solicitante, guia, asistentes, periodo, laboratorioId, fecha, horaIngreso, horaSalida } = await request.json()
-    const [result, fields] = await db.query('INSERT INTO tbl_solicitud_externa ( TIR_ID, PER_ID, SOE_GUIA, SOE_ASISTENTES, PEA_ID, LAB_ID, SOE_FECHA, SOE_HORA_INGRESO, SOE_HORA_SALIDA) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', [tipoSolicitud, solicitante, guia, asistentes, periodo, laboratorioId, fecha, horaIngreso, horaSalida])
+    const { solicitante, guia, asistentes, periodo, laboratorioId, fecha, horaIngreso, horaSalida } = await request.json()
+    const [result, fields] = await db.query('INSERT INTO tbl_solicitud_externa ( PER_ID, SOE_GUIA, SOE_ASISTENTES, PEA_ID, LAB_ID, SOE_FECHA, SOE_HORA_INGRESO, SOE_HORA_SALIDA) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [solicitante, guia, asistentes, periodo, laboratorioId, fecha, horaIngreso, horaSalida])
     db.end()
     if (result.affectedRows > 0) {
         return NextResponse.json({ msg: "Solicitud registrada", estado: true }, { status: 201 })
