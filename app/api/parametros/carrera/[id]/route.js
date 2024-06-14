05fa311c080fcc37bc1551c7af2b2fb7a5c3fbc3
AndryAmagua <andryamagua5@gmail.com>
2024-06-14 01:09:37 -0500

    api: get carrera [id]

--- app/api/parametros/carrera/[id]/route.js
+++ app/api/parametros/carrera/[id]/route.js
@@ -1,6 +1,20 @@
 import { NextResponse } from 'next/server';
 import openDB from '@/config/db'
 
+export async function GET(request, { params }) {
+    const db = await openDB()
+    if (db.estado === undefined) {
+        db.connect()
+        const id = await params.id
+        const [rows, fields] = await db.query('SELECT * FROM tbl_carrera WHERE carrera_id = ?', [id]);
+        db.end()
+        return NextResponse.json({ data: rows }, { status: 200 })
+
+    } else {
+        return NextResponse.json({ data: [] }, { status: 500 })
+    }
+}
+
 export async function DELETE(request, { params }) {
     const db = await openDB()
     db.connect()
