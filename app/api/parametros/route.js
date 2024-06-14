1e6745dc950730960ac33c994af774d669e16e9f
AndryAmagua <andryamagua5@gmail.com>
2024-06-13 23:55:26 -0500

    api: parametros

--- app/api/parametros/route.js
+++ app/api/parametros/route.js
@@ -1,13 +1,16 @@
 import { NextResponse } from 'next/server';
 import openDB from '@/config/db'
 
-export async function GET(request) {
+export async function POST(request) {
     const db = await openDB()
     if (db.estado === undefined) {
+        const { carrera_id } = await request.json()
+
         db.connect()
-        const [rows] = await db.query('SELECT * FROM tbl_carrera WHERE estado = 1')
-        const [rows2] = await db.query('SELECT * FROM tbl_periodo_academico WHERE estado = 1')
-        const [rows3] = await db.query('SELECT * FROM tbl_asignatura WHERE estado = 1')
+        const [rows] = await db.query('SELECT * FROM tbl_periodo_academico WHERE carrera_id = ? AND estado = 1', [carrera_id])
+        const [rows2] = await db.query('SELECT * FROM tbl_asignatura WHERE carrera_id = ? AND estado = 1', [carrera_id])
+        const [rows3] = await db.query('SELECT * FROM tbl_ejecucion_practica WHERE carrera_id = ? AND estado = 1', [carrera_id])
+
         db.end()
         return NextResponse.json({ data: [rows, rows2, rows3] }, { status: 200 })
 
