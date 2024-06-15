fae6022e7ee1602a5d784064b9bec1bd36d46373
AndryAmagua <andryamagua5@gmail.com>
2024-06-15 18:41:24 -0500

    api: correct table updated

--- app/api/reserva/editar/[id]/route.js
+++ app/api/reserva/editar/[id]/route.js
@@ -6,7 +6,7 @@ export async function PUT(request, { params }) {
     db.connect()
     const id = await params.id
     const { periodo_id, modalidad, asignatura_id, area_id, aula, nivel, paralelo, fecha, horaInicio, horaFin, temaGuia, detalle } = await request.json()
-    const [result, fields] = await db.query('UPDATE tbl_solicitud_reserva SET periodo_id = ?, modalidad = ?, asignatura_id = ?, area_id = ?, aula = ?, nivel = ?, paralelo = ?, fecha = ?, horaInicio = ?, horaFin = ?, temaGuia = ?, detalle = ?, estado_id = 1 WHERE solicitud_id = ?', [periodo_id, modalidad, asignatura_id, area_id, aula, nivel, paralelo, fecha, horaInicio, horaFin, temaGuia, detalle, id]);
+    const [result, fields] = await db.query('UPDATE tbl_solicitud_interna SET periodo_id = ?, modalidad = ?, asignatura_id = ?, area_id = ?, aula = ?, nivel = ?, paralelo = ?, fecha = ?, horaInicio = ?, horaFin = ?, temaGuia = ?, detalle = ?, estado_id = 1 WHERE solicitud_id = ?', [periodo_id, modalidad, asignatura_id, area_id, aula, nivel, paralelo, fecha, horaInicio, horaFin, temaGuia, detalle, id]);
     db.end()
     if (result.changedRows > 0) {
         return NextResponse.json({ msg: "Solicitud editada", estado: true }, { status: 202 })
