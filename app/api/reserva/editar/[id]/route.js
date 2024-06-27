76cc7bc210637c1919448ce31385e683cc63962e
AndryAmagua <andryamagua5@gmail.com>
2024-06-27 10:17:59 -0500

    set ejecucion tbl_solicitud_interna

--- app/api/reserva/editar/[id]/route.js
+++ app/api/reserva/editar/[id]/route.js
@@ -5,8 +5,8 @@ export async function PUT(request, { params }) {
     const db = await openDB()
     db.connect()
     const id = await params.id
-    const { periodo_id, modalidad, asignatura_id, especialidad_id, area_id, aula, nivel, paralelo, fecha, horaInicio, horaFin, temaGuia, detalle } = await request.json()
-    const [result, fields] = await db.query('UPDATE tbl_solicitud_interna SET periodo_id = ?, modalidad = ?, asignatura_id = ?, especialidad_id = ?, area_id = ?, aula = ?, nivel = ?, paralelo = ?, fecha = ?, horaInicio = ?, horaFin = ?, temaGuia = ?, detalle = ?, estado_id = 1 WHERE solicitud_id = ?', [periodo_id, modalidad, asignatura_id, especialidad_id, area_id, aula, nivel, paralelo, fecha, horaInicio, horaFin, temaGuia, detalle, id]);
+    const { periodo_id, modalidad, asignatura_id, especialidad_id, area_id, aula, nivel, paralelo, fecha, horaInicio, horaFin, ejecucion_id, temaGuia, detalle } = await request.json()
+    const [result, fields] = await db.query('UPDATE tbl_solicitud_interna SET periodo_id = ?, modalidad = ?, asignatura_id = ?, especialidad_id = ?, area_id = ?, aula = ?, nivel = ?, paralelo = ?, fecha = ?, horaInicio = ?, horaFin = ?, ejecucion_id = ?, temaGuia = ?, detalle = ?, estado_id = 1 WHERE solicitud_id = ?', [periodo_id, modalidad, asignatura_id, especialidad_id, area_id, aula, nivel, paralelo, fecha, horaInicio, horaFin, ejecucion_id, temaGuia, detalle, id]);
     db.end()
     if (result.changedRows > 0) {
         return NextResponse.json({ msg: "Solicitud editada", estado: true }, { status: 202 })
